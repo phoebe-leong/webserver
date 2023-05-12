55e6ded50a9198bb24c0625c4448d4890177b252
ph3b3 <dariusleong8@gmail.com>
2023-05-12 19:02:37 +1000

    Add files via upload

---
+++ webserver.js
@@ -0,0 +1,49 @@
+const path = process.argv[2] || __dirname
+const errPath = process.argv[3] || path
+
+const express = require("express")
+const app = express()
+
+const fs = require("fs")
+
+function send(file, loc) {
+	fs.readFile(file, "utf-8", (error, data) => {
+		if (error) {
+			console.error(error)
+
+			fs.readFile(`${errPath}500.html`, (error, data) => {
+				if (error) { loc.send("Error 500: Error while displaying message in response to previous error") }
+				else { loc.send(data) }
+			})
+		}
+		else {
+			loc.send(data)
+		}
+	})
+}
+
+app.get("/", (req, res) => {
+	res.set("Content-Type", "text/html")
+	send(`${path}/index.html`, res)
+})
+app.get("/style.css", (req, res) => {
+	res.set("Content-Type", "text/css")
+	send(`${path}/style.css`, res)
+})
+app.get("/script.js", (req, res) => {
+	res.set("Content-Type", "application/javascript")
+	send(`${path}/script.js`, res)
+})
+app.get("/favicon.png", (req, res) => {
+	res.set("Content-Type", "image/png")
+	res.sendFile(`${path}/favicon.png`)
+})
+
+app.use((req, res) => {
+	fs.readFile(`${errPath}/404.html`, "utf-8", (error, data) => {
+		if (error) { console.error(error) }
+		else { res.status(404).send(data) }
+	})
+})
+
+app.listen(8000, () => console.log("Listening on port 8000"))
